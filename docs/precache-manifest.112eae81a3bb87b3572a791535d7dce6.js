9949cb92dfc32a1ef35a2745ca398c2ea9e3eecd
gabrielg21 <gabo21gonzalez@gmail.com>
2020-08-29 21:55:05 -0600

    docs agregado

---
+++ docs/precache-manifest.112eae81a3bb87b3572a791535d7dce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15a128ed0ede38339a99b04c01be27b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c858c750218f59e6e18b",
+    "url": "/static/css/main.b8d829d0.chunk.css"
+  },
+  {
+    "revision": "b617679593a3f0bd5eaa",
+    "url": "/static/js/2.eea0565d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eea0565d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c858c750218f59e6e18b",
+    "url": "/static/js/main.ffb6eba1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
